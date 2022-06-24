e7d4ffb833504a1cc9b969aa4ca956f9f32bc115
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-06-24 14:47:58 +0800

    removing_cookies

--- removing_cookies.php
+++ removing_cookies.php
@@ -13,8 +13,8 @@
        print ("(<pre>\n");
        print ("2 cookies were delivered with past times. \n");
 
-       $count = count($_COOKIE) ;
-       print ("Scount cookies received. \n"):
+       $count = count($_COOKIE);
+       print ("$count cookies received. \n"):
        foreach ($_COOKIE as $name => $value){
             print "$name = $value\n";
        }
